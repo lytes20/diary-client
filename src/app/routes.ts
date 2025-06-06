4c8b256dc579ffd07ecd673b706ad7a1f6b431e2
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-06-05 22:48:06 -0500

    Add Diaries component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,10 @@
 import { Route, Routes } from '@angular/router';
 import { Signin } from './user/signin';
 import { Signup } from './user/signup';
+import { Diary } from './diary/diary';
 
 export const routes: Routes = [
   { path: '', component: Signin },
   { path: 'signup', component: Signup },
+  { path: 'diaries', component: Diary },
 ];
--- src/app/user/signin.ts
+++ src/app/user/signin.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: 'signin.html',
   styleUrl: './sign.css',
 })
-export class Signin {}
+export class Signin {
+  #router = inject(Router);
+  handleLogin() {
+    this.#router.navigate(['diaries']);
+  }
+}
